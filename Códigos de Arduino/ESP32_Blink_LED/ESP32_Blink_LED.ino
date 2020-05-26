d23d2fa826de2f7ef40043fa9fe4f73795f0d572
AndresCans <cardemas21@gmail.com>
2020-05-26 11:03:11 -0500

    actualizacion de programa

--- Códigos de Arduino/ESP32_Blink_LED/ESP32_Blink_LED.ino
+++ Códigos de Arduino/ESP32_Blink_LED/ESP32_Blink_LED.ino
@@ -1,18 +1,18 @@
 //Para hacer esta prueba de funcionamiento con el ESP32, 
 //haremos parpadear el LED interno de la tarjeta que es el pin 2
 
-int LED_PRUEBA = 2;
+int LED_BUILTIN = 2;
 
 void setup() {
-  pinMode (LED_PRUEBA, OUTPUT);
+  pinMode (LED_BUILTIN, OUTPUT);
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  digitalWrite(LED_PRUEBA, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
-  digitalWrite(LED_PRUEBA; LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
   
   // put your main code here, to run repeatedly:
